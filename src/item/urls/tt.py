b0cd9868b766332b1680d52a80d644ec1d8a25fe
Alamin <miraajehossain@gmail.com>
2021-06-18 04:18:59 +0600

    removed unused file

--- src/item/urls/tt.py
+++
@@ -1,5 +0,0 @@
-import datetime
-date= datetime.datetime.now().date()
-year, month, day= str(date).split('-')
-day_name = datetime.date(int(year), int(month), int(day))
-print(day_name.strftime("%A"))
