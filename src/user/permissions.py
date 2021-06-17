8185f56529ec07d7fba3726c64723d832a577fb5
Alamin <miraajehossain@gmail.com>
2021-06-18 01:13:43 +0600

    add restaurant owner permission

--- src/user/permissions.py
+++ src/user/permissions.py
@@ -4,3 +4,9 @@ from rest_framework import permissions
 class IsSuperUser(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.is_superuser
+
+
+class IsRestaurantOwner(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return 'RestaurantOwner' in request.groups
+
