7ef8fbcfab3c3215be6bfeaa6ffa9178c6815af9
Alamin <miraajehossain@gmail.com>
2021-06-24 23:52:59 +0600

    get user restaurant details api done

--- src/restaurant/views/users.py
+++ src/restaurant/views/users.py
@@ -1,8 +1,10 @@
 
-from rest_framework import permissions
+from rest_framework import permissions, status
 from rest_framework.generics import (
     RetrieveAPIView,
 )
+from rest_framework.response import Response
+
 from restaurant.models import Restaurant
 from restaurant.serializers import (
     RestaurantSerializer,
@@ -11,6 +13,20 @@ from restaurant.serializers import (
 
 class RestaurantGetAPIView(RetrieveAPIView):
     permission_classes = [permissions.IsAuthenticated]
-    queryset = Restaurant.objects.filter()
-    lookup_field = 'slug'
-    serializer_class = RestaurantSerializer
+
+    def get(self, request, *args, **kwargs):
+        try:
+            slug = kwargs.get('slug')
+            restaurant = Restaurant.objects.get(slug=slug)
+
+            return Response({
+                'success': True,
+                'message': 'restaurant details',
+                'data': RestaurantSerializer(restaurant).data
+            }, status=status.HTTP_200_OK)
+
+        except Restaurant.DoesNotExist:
+            return Response({
+                'success': False,
+                'message': 'restaurant does not found',
+            }, status=status.HTTP_400_BAD_REQUEST)
